7be3c5c9ec8d9e21e16fafc6b268b3619552fe53
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2025-09-01 17:30:50 +0500

    first working version

--- main.ts
+++ main.ts
@@ -1,134 +1,82 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import { Editor, MarkdownView, Notice, Plugin, TAbstractFile, TFile } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+import { DataviewApi, getAPI } from 'obsidian-dataview';
+
+// https://regex101.com/r/i9dmNu/4
+const matchDataviewCodeblockRegex = /```inline dataview\n((?:[\s\S](?!```))*)\n```(?:\n*<!--DATAVIEW INLINE START-->(?:[\s\S](?!<!--))*\n<!--DATAVIEW INLINE END-->)?/g;
+
+async function replaceAsync(string: string, regexp: RegExp, replacerFunction: (...args: unknown[]) => Promise<string>) {
+    const replacements = await Promise.all(
+        Array.from(string.matchAll(regexp),
+            match => replacerFunction(...match)));
+    let i = 0;
+    return string.replace(regexp, () => replacements[i++]);
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class DataviewInlinePlugin extends Plugin {
+	modifiedFiles: Set<TAbstractFile> = new Set();
+	files: Set<TAbstractFile> = new Set();
+	dataviewAPI: DataviewApi;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	async updateFile(file: TAbstractFile) {
+		if (!(file instanceof TFile)) {return;}
 
+		let fileContent = await file.vault.read(file);
+
+		fileContent = await replaceAsync(fileContent, matchDataviewCodeblockRegex, async (substr, content: string) => {
+			const codeblock = `\`\`\`inline dataview\n${content}\n\`\`\``;
+			const rendered = await this.dataviewAPI.tryQueryMarkdown(content, fileContent);
+			return `${codeblock}\n<!--DATAVIEW INLINE START-->\n\n${rendered}\n<!--DATAVIEW INLINE END-->`
+		});
+
+		this.modifiedFiles.add(file);
+		file.vault.modify(file, fileContent);
+	}
+
+	async sheduleFileUpdate(file: TAbstractFile): Promise<void> {
+		if (this.modifiedFiles.has(file)) {
+			this.modifiedFiles.delete(file);
+			return;
+		}
+		this.files.add(file)
+	}
+	
 	async onload() {
-		await this.loadSettings();
+		const dataviewAPI = getAPI(this.app);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (!dataviewAPI) {
+			const errMessage = 'The Dataview plugin is not installed or enabled. Please make sure it is installed and enabled, then restart Obsidian';
+			console.log(errMessage);
+			new Notice(errMessage, 0);
+			return;
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.dataviewAPI = dataviewAPI;
 
-		// This adds a simple command that can be triggered anywhere
+		this.registerEvent(
+			this.app.vault.on("modify", (file) => this.sheduleFileUpdate(file))
+		)
+
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'dataview-inline-scan',
+			name: 'Scan',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (view.file) {
+					this.updateFile(view.file)
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
+		this.registerInterval(window.setInterval(() => {
+			this.files.forEach(file => this.updateFile(file))
+			this.files.clear();
+		}, 2000))
 	}
+
+	
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
